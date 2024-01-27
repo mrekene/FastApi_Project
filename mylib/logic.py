6309887249b20608d632f6fe1b03a7826c1609ff
Ekene.Okpara <147102794+mrekene@users.noreply.github.com>
2024-01-27 17:20:42 +0000

    adding a new cli functionality

--- mylib/logic.py
+++ mylib/logic.py
@@ -5,3 +5,10 @@ def wiki(name="war Goddess", length=1):
     """This is a wikipedia fetcher"""
     my_wiki = wikipedia.summary(name, length)
     return my_wiki
+
+def search_wiki(name):
+    """Search Wikipedia for Names"""
+
+    results = wikipedia.search(name)
+    return results
+
